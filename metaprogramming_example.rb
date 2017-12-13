94c214d1bb601da52a3a8ada4c7c91c785f62365
Kyle Hatch <ky13h4tch@gmail.com>
2017-12-13 10:37:52 -0700

    Metaprogramming examples

--- metaprogramming_example.rb
+++ metaprogramming_example.rb
@@ -1,4 +1,16 @@
-class Pudge
+class Invoker
 end
 
-p Pudge.new.hook
+class Invoker
+  def eee
+    "Sunstrike"
+  end
+end
+
+class Invoker
+  def eee
+    "This is not Deafening Blast"
+  end
+end
+
+p Invoker.new.eee
